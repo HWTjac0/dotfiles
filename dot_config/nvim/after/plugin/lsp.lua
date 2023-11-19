c036af9de6f601bea59f3974c4a3e8cc2c124f2f
HWTjac0 <jakub3020.jmy@gmail.com>
2023-11-19 19:23:54 +0100

    neovim changes

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -8,7 +8,7 @@ require('mason').setup({
 local lsp = require("lsp-zero")
 
 require("lspconfig").emmet_language_server.setup {
-  filetypes = {"html", "php"}
+  filetypes = { "html", "php" }
 }
 
 require("lspconfig").texlab.setup {
@@ -20,6 +20,21 @@ require("lspconfig").texlab.setup {
 require("lspconfig").phpactor.setup {
 }
 
+require("lspconfig").tsserver.setup {
+  init_options = {
+    preferences = {
+      includeInlayParameterNameHints = "all",
+      includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+      includeInlayFunctionParameterTypeHints = true,
+      includeInlayVariableTypeHints = true,
+      includeInlayPropertyDeclarationTypeHints = true,
+      includeInlayFunctionLikeReturnTypeHints = true,
+      includeInlayEnumMemberValueHints = true,
+      importModuleSpecifierPreference = 'non-relative'
+    },
+  },
+}
+
 lsp.preset("recommended")
 
 lsp.ensure_installed({
@@ -29,6 +44,13 @@ lsp.ensure_installed({
 
 lsp.setup()
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    vim.lsp.inlay_hint.enable(bufnr, true)
+  end
+})
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,
--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -15,25 +15,30 @@ require('lazy').setup({
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v1.x',
     dependencies = {
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'neovim/nvim-lspconfig' },
+      { 'williamboman/mason.nvim' },
+      { 'williamboman/mason-lspconfig.nvim' },
 
 
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'L3MON4D3/LuaSnip'},
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
 
-      {'nvim-treesitter/nvim-treesitter', cmd = "TSUpdate"},
+      { 'nvim-treesitter/nvim-treesitter',  cmd = "TSUpdate" },
 
-      {'nvim-tree/nvim-web-devicons'},
       {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
         dependencies = { 'nvim-lua/plenary.nvim' }
       },
-      {'nvim-tree/nvim-tree.lua'},
-      { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-      {'nvim-lualine/lualine.nvim'}
-    }
-  }
+    },
+  },
+  { 'nvim-lualine/lualine.nvim' },
+  { "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+  },
+  { 'nvim-tree/nvim-web-devicons' },
+  { 'nvim-tree/nvim-tree.lua' },
 })
